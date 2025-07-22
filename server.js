3ef5fd03e04d1ac88e3e5cac928b6a0ed7fda143
pericarney00 <pericarney@gmail.com>
2025-07-22 10:52:42 -0500

    Established controller folder

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 
+const authController = require("./controllers/auth.js")
+
+
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
 
@@ -31,7 +34,7 @@ app.get("/", async(req, res) => {
   res.render("index.ejs")
 })
 
-
+app.use("/auth", authController);
 
 
 
